2ae307ba7f387dfb45148c46531652cd150d70dd
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2025-03-28 17:59:53 +0300

    feat(tanstack/query): connect provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
+
 function App() {
 
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
             <h1 className="underline">
                 123
             </h1>
-        </>
+        </QueryClientProvider>
     )
 }
 
