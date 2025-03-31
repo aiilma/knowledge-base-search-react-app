7a8a7f6d19534095a99551e8656c5a76af6b22af
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2025-03-31 17:22:57 +0300

    feat(react-query-devtools): setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -6,6 +7,8 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <h1 className="underline">123</h1>
+
+      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   )
 }
