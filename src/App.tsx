37edaaac46045a622208a476c142e201cc0bc984
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2025-03-31 19:18:42 +0300

    feat(package.json): +react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter as Router } from 'react-router'
 import ArticleSearchView from './views/ArticleSearchView.tsx'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
@@ -7,8 +8,9 @@ const queryClient = new QueryClient()
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ArticleSearchView />
-
+      <Router>
+        <ArticleSearchView />
+      </Router>
       <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   )
