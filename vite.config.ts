72c1777b8c051847f14df5af13b245e9d0730d7c
Vitaly Mortikov <vitaly.mortikov@mail.ru>
2025-04-01 08:35:33 +0300

    fix: prettier + lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-      react(),
-      tailwindcss(), // https://tailwindcss.com/docs/installation/using-vite
+    react(),
+    tailwindcss() // https://tailwindcss.com/docs/installation/using-vite
   ],
 })
